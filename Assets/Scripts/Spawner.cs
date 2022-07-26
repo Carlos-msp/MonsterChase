94176e53912220705fb24e249c40e3251fb007f8
Carlos Pinto <carlos.pinto@exaud.com>
2022-07-26 10:24:24 +0100

    Spawn done

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,31 +6,41 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] enemyTypes;
-    [SerializeField]
-    private Transform[] spawnPositions;
+
+    private List<Transform> spawnPositions;
 
 
     // Start is called before the first frame update
     void Start()
     {
-         StartCoroutine(SpawnMonsters());
+        spawnPositions = new List<Transform>();
+        foreach (Transform childTransform in transform)
+        {
+            spawnPositions.Add(childTransform);
+        }
+        StartCoroutine(SpawnMonsters());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    IEnumerator SpawnMonsters() {
-        yield return new WaitForSeconds(Random.Range(1,5));
+    IEnumerator SpawnMonsters()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
 
-        int randomIndex = Random.Range(0, enemyTypes.Length);
-        GameObject spawnedMonster = Instantiate(enemyTypes[randomIndex]);
-        randomIndex = Random.Range(0, spawnPositions.Length);
-        Transform spawnPos = spawnPositions[randomIndex];
+            int randomIndex = Random.Range(0, enemyTypes.Length);
+            GameObject spawnedMonster = Instantiate(enemyTypes[randomIndex]);
+            randomIndex = Random.Range(0, spawnPositions.Count);
+            Transform spawnPos = spawnPositions[randomIndex];
+            Debug.Log("Spawned at {x: " + spawnPos.position.x + "y: " + spawnPos.position.y + "z: " + spawnPos.position.z + "}");
 
-        spawnedMonster.transform.position = spawnPos.position;
-        spawnedMonster.GetComponent<Enemy>().speed = Random.Range(1f, 3f);
+            spawnedMonster.transform.position = spawnPos.position;
+            spawnedMonster.GetComponent<Enemy>().speed = Random.Range(1f, 3f);
+        }
     }
 }
